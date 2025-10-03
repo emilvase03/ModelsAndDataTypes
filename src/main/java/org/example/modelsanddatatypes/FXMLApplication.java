80521fbc3522f5cc136b2d73c8dbd5a8e6e038c4
murik <dennyisgodxd@gmail.com>
2025-10-03 09:28:03 +0200

    fixed view naming

--- src/main/java/org/example/modelsanddatatypes/FXMLApplication.java
+++ src/main/java/org/example/modelsanddatatypes/FXMLApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class FXMLApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FXMLApplication.class.getResource("/views/NumberConversion.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FXMLApplication.class.getResource("/views/NumberConversionView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
